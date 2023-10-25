58551c1ad2db307fc473404b4129270009fc2ac1
Léo Deschamps <ldeschamps41@free.fr>
2023-10-25 15:51:20 +0200

    test de la configuration

---
+++ main.py
@@ -0,0 +1,26 @@
+import pymysql
+from app import app
+from config import mysql
+from flask import jsonify
+from flask import flash, request
+
+@app.route("/test")
+def test():
+    try:
+        conn = mysql.connect()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        cursor.execute("SELECT id, title FROM films")
+        empRows = cursor.fetchall()
+        respone = jsonify(empRows)
+        respone.status_code = 200
+        return respone
+    except Exception as e:
+        print("ddddddddddd")
+        print(e)
+    finally:
+        cursor.close() 
+        conn.close()  
+
+
+if __name__ == "__main__":
+    app.run()
