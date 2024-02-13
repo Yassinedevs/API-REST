8cafbabbb26f695e3b84d3706b4b30b9e2a357dd
Yassinedevs <122100038+Yassinedevs@users.noreply.github.com>
2024-02-13 09:00:12 +0100

    starships commit

--- routes/routes_starships.py
+++ routes/routes_starships.py
@@ -3,9 +3,16 @@ from config import mysql
 from flask import jsonify
 from flask import flash, request
 from flask import Blueprint
-from flask_restx import Resource, Namespace
+from flask_restx import Resource, Namespace, fields
 
 starships_namespace = Namespace('starships', description='Endpoints pour les starships')
+
+starship_model = starships_namespace.model('StarshipModel', {
+    'MGLT': fields.String(required=True, description='La vitesse Megalumière par heure'),
+    'starshipClass': fields.String(description='Le type de vaisseau'),
+    'hyperdriveRating': fields.String(description='Catégorie de vitesse'),
+    'idTransport': fields.String(description="L'ID technique du vaisseau"),
+})
 
 
 @starships_namespace.route("")
@@ -14,7 +21,14 @@ class StarshipsResource(Resource):
         try:
             conn = mysql.connect()
             cursor = conn.cursor(pymysql.cursors.DictCursor)
-            cursor.execute("SELECT starship_class FROM starships")
+            query = '''
+                    SELECT t.name, t.model, t.manufacturer, t.costInCredits, t.length, t.maxAtmospheringSpeed, t.crew, t.passengers, t.cargoCapacity, t.consumables, s.hyperdriveRating, s.MGLT, s.starshipClass, p.idPeople as "pilots", fs.idFilm as "films", t.created, t.edited, s.idStarship as "url"
+                    FROM starships s
+                    LEFT JOIN transport t ON s.idTransport = t.idTransport
+                    LEFT JOIN filmsstarships fs ON s.idStarship = fs.idStarship
+                    LEFT JOIN people p ON s.idStarship = p.idStarship;
+                '''
+            cursor.execute(query)
             film_rows = cursor.fetchall()
 
             if film_rows:
@@ -48,9 +62,39 @@ class StarshipsResource(Resource):
         response.status_code = status_code
         return response
 
+    @starships_namespace.expect(starship_model)
     def post(self):
-        return {"create":"a faire"}
+        try :
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            query = '''
+                    INSERT INTO starships (MGLT, starshipClass, hyperdriveRating, idTransport)
+                    VALUES  (%s, %s, %s, %s)
+                '''
+ 
+            values = (
+                json_data.get('MGLT'),
+                json_data.get('starshipClass'),
+                json_data.get('hyperdriveRating'),
+                json_data.get('idTransport'),
+            )
 
+            cursor.execute(query, values)
+
+        except Exception as e:
+            response_data = {
+                "status": "error",
+                "message": "Échec de la suppression du starship.",
+                "error_code": "INTERNAL_SERVER_ERROR",
+                "details": str(e)
+            }
+            status_code = 500
+        finally:
+            cursor.close() 
+            conn.close()  
+
+ 
+        return {'message': 'Ajout du vaisseau avec succès'}
 
 @starships_namespace.route("/<int:id>")
 class StarshipResource(Resource):
@@ -58,7 +102,15 @@ class StarshipResource(Resource):
         try:
             conn = mysql.connect()
             cursor = conn.cursor(pymysql.cursors.DictCursor)
-            cursor.execute("SELECT starship_class FROM starships WHERE id = %s", (id,))
+            query = '''
+                    SELECT t.name, t.model, t.manufacturer, t.costInCredits, t.length, t.maxAtmospheringSpeed, t.crew, t.passengers, t.cargoCapacity, t.consumables, s.hyperdriveRating, s.MGLT, s.starshipClass, p.idPeople as "pilots", fs.idFilm as "films", t.created, t.edited, s.idStarship as "url"
+                    FROM starships s
+                    LEFT JOIN transport t ON s.idTransport = t.idTransport
+                    LEFT JOIN filmsstarships fs ON s.idStarship = fs.idStarship
+                    LEFT JOIN people p ON s.idStarship = p.idStarship
+                    WHERE s.idStarship = %s;
+                '''
+            cursor.execute(query, (id,))
             film_rows = cursor.fetchall()
 
             if film_rows:
@@ -93,9 +145,78 @@ class StarshipResource(Resource):
         return response
 
     def delete(self, id):
-        return {"create":"a faire"}
+        try:
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM starships WHERE idStarship = %s", (id,))
+            film_rows = cursor.fetchall()
 
+            if film_rows :
+                cursor = conn.cursor()
+                cursor.execute("DELETE FROM filmsstarships WHERE idStarship = %s", (id,))
+                cursor.execute("DELETE FROM starships WHERE idStarship = %s", (id,))
+                response_data = {
+                    "status": "success",
+                    "message": "starships supprimé avec succès.",
+                    "data": film_rows
+                }
+                status_code = 200
+            else :
+                response_data = {
+                    "status": "error",
+                    "message": "Aucun starships trouvé.",
+                    "data": []
+                }
+                status_code = 404
+
+        except Exception as e:
+            response_data = {
+                "status": "error",
+                "message": "Échec de la suppression du starship.",
+                "error_code": "INTERNAL_SERVER_ERROR",
+                "details": str(e)
+            }
+            status_code = 500
+        finally:
+            cursor.close() 
+            conn.close()  
+
+        response = jsonify(response_data)
+        response.status_code = status_code
+        return response
+
+    @starships_namespace.expect(starship_model)
     def put(self, id):
-        return {"create":"a faire"}
+        try :
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            query = '''
+                    UPDATE starships
+                    SET MGLT = %s, starshipClass = %s, hyperdriveRating = %s, idTransport = %s
+                    WHERE idStarship = %s
+                '''
+ 
+            values = (
+                json_data.get('MGLT'),
+                json_data.get('starshipClass'),
+                json_data.get('hyperdriveRating'),
+                json_data.get('idTransport'),
+                id
+            )
 
+            cursor.execute(query, values)
 
+        except Exception as e:
+            response_data = {
+                "status": "error",
+                "message": "Échec de la suppression du starship.",
+                "error_code": "INTERNAL_SERVER_ERROR",
+                "details": str(e)
+            }
+            status_code = 500
+        finally:
+            cursor.close() 
+            conn.close()  
+
+ 
+            return {'message': 'Mise à jour du vaisseau avec succès'}
